2040f31b7f99a6f4a0b4758004f29dc07da3f99d
jdavid80 <jdcalderong@gmail.com>
2024-06-29 00:31:30 -0500

    add convocatoria

--- js/index.js
+++ js/index.js
@@ -11,3 +11,22 @@ menuBarra.addEventListener("click", function() {
         mainMenuMovil.classList.remove("main-menu-movil");
     }
 });
+
+let mainNotice = document. getElementById("mainNotice");
+let imgNotice = document.getElementById("imgNotice");
+let btnCloseNotice = document.getElementById("btnCloseNotice");
+
+btnCloseNotice.addEventListener("click",function() {
+    if (mainNotice.classList.contains("main-notice-visible")) {
+        mainNotice.classList.add("main-notice-hidden");
+        mainNotice.classList.remove("main-notice-visible");
+        imgNotice.classList.add("img-notice-hidden");
+        imgNotice.classList.remove("img-notice-visible");
+    }
+/*     else {
+        mainNotice.classList.remove("main-notice-hidden");
+        mainNotice.classList.add("main-notice-visible");
+        imgNotice.classList.remove("img-notice-hidden");
+        imgNotice.classList.add("img-notice-visible");
+    } */
+});
